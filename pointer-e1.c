1c06c39c04c4f077c1ea5f66b12e422035d3172c
lzxsdsz <3123755575@qq.com>
2023-04-15 00:54:57 +0800

    指针进阶(1)

--- pointer-e1.c
+++ pointer-e1.c
@@ -14,9 +14,9 @@ int main() {
 	char arr2[] = "acbdef";
 	char* p1 = "abedef";
 	char* p2 = "abcdef";
-	if (arr1 == arr2){
+	if (arr1 == arr2) {
 		printf("hehe\n");
-		}
+	}
 	else
 	{
 		printf("haha\n");
@@ -53,7 +53,7 @@ int main() {
 	int b = 20;
 	int c = 30;
 	int d = 40;
-	int* arr[4] = { &a,&b,&c,&d }; 
+	int* arr[4] = { &a,&b,&c,&d };
 	return 0;
 }
 int main() {
@@ -103,7 +103,7 @@ int main() {
 	char* (*pa)[5] = &arr;
 
 	int arr2[10] = { 0 };
-	int(*pa2)[10] = &arr2; 
+	int(*pa2)[10] = &arr2;
 	return 0;
 }
 
@@ -118,26 +118,26 @@ void print1(int arr[3][5], int x, int y) {
 		printf("\n");
 	}
 }
-void print2(int (*p)[5], int x, int y) {
+void print2(int(*p)[5], int x, int y) {
 	int i = 0;
 	for (i = 0; i < x; i++) {
 		int j = 0;
 		for (j = 0; j < y; j++) {
-			printf("%d ", *(*(p + i) + j))   ;
+			printf("%d ", *(*(p + i) + j));
+		}
 	}
-}
-int main() {
-	int arr[3][5] = { {1,2,3,4,5}, {2,3,4,5,6},{3,4,5,6,7} };
-	print1(arr, 3, 5);
-	print2(arr, 3, 5);
-	return 0;
-}
+	int main() {
+		int arr[3][5] = { {1,2,3,4,5}, {2,3,4,5,6},{3,4,5,6,7} };
+		print1(arr, 3, 5);
+		print2(arr, 3, 5);
+		return 0;
+	}
 
-int arr[5];//arr是一个5个元素的整型数组
-int* parr1[10];//parr1是一个数组，数组有10个元素
-			   //每个元素的类型是int* parr1是指针数组
-int(*parr2)[10];//parr2是一个指针，该指针指向了一个元素
-				//数组有110个元素，每个元素类型是int
-int(*(parr3[10]))[5];
-//parr3是一个数组，该数组有10个元素，每个元素是一个数组指针
-//该数组指针指向的数组有5个元素,每个元素是int
+	int arr[5];//arr是一个5个元素的整型数组
+	int* parr1[10];//parr1是一个数组，数组有10个元素
+	//每个元素的类型是int* parr1是指针数组
+	int(*parr2)[10];//parr2是一个指针，该指针指向了一个元素
+	//数组有110个元素，每个元素类型是int
+	int(*(parr3[10]))[5];
+	//parr3是一个数组，该数组有10个元素，每个元素是一个数组指针
+	//该数组指针指向的数组有5个元素,每个元素是int
